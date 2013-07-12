1c69639501aaf7ad4d252298c834a5426b84b720
donsalari <ameyakoshti@gmail.com>
2013-07-12 16:21:38 -0700

    Initial Commit

---
+++ facebook_login.php
@@ -0,0 +1,18 @@
+<html>
+<meta charset="UTF-8">
+	<head>
+		<title>Facebook Login</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<link rel="stylesheet" href="css/bootstrap.css" />
+		<link rel="stylesheet" href="css/bootstrap-responsive.css" />
+		<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<h1> I got here!! </h1>
+		<?php		
+		error_reporting(E_PARSE);
+		
+		echo "Hi";
+		?>
+	</body>
+</html>
