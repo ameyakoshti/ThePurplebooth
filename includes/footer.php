a3b724b977f90c2f514940f27ba965d6521a19b8
anetfreak <anetfreak@gmail.com>
2013-09-01 13:26:31 -0700

    New footer... :)

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,8 @@
 <!-- FOOTER -->
-<div id="footer" class="footer" style="margin: 20px;">
-	<footer>
-		<hr class="featurette-divider">
+<div id="footer" style="background-color: #000000; margin-top: 20px;">
+	<div class="container" style="padding-top: 20px;">
 		<p class="pull-right"><a href="#">Back to top</a></p>
-		<p>	Copyright 2013, The Purple Booth, Inc. | <a href="#">Privacy</a> | <a href="#">Terms</a> | <a href="/codenameDS/contact.php">Contact</a> | <a href="/codenameDS/about_us.php">About Us</a> </p>
-	</footer>
+    	<p>	Copyright 2013, The Purple Booth, Inc. | <a href="#">Privacy</a> | <a href="#">Terms</a> | <a href="/codenameDS/contact.php">Contact</a> | <a href="/codenameDS/about_us.php">About Us</a> </p>
+    </div>
 </div>
 <!-- FOOTER -->
