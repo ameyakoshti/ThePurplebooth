7cca8e2c504b7bd5cd601c65371db4056c5fbd75
donsalari <ameyakoshti@gmail.com>
2013-09-03 01:18:35 -0700

    footer changed

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,31 @@
 <!-- FOOTER -->
 <div id="footer">
 	<div class="container" >
-		<p class="pull-right"><a href="#">Back to top</a></p>
+		<!--<p class="pull-right"><a href="#">Back to top</a></p>
     	<p>	Copyright 2013, The Purple Booth, Inc. | <a href="#">Privacy</a> | <a href="#">Terms</a> | <a href="/codenameDS/contact.php">Contact</a> | <a href="/codenameDS/about_us.php">About Us</a> </p>
+    -->    
+	    <div class="row-fluid">
+			<div class="span4">
+				<h4>More about us</h4>
+				<p>
+			 		<a href="/codenameDS/contact.php">Contact</a></br>
+			 		<a href="/codenameDS/about_us.php">About Us</a>
+				</p>
+			</div>
+			<div class="span4">
+				<h4>Follow Us</h4>
+				<p>
+					Get the list of the <a href="#">partners here</a>
+				</p>
+			</div>
+			<div class="span4">
+				<h4>Boring Stuff</h4>
+				<p>
+					Copyright © 2013 We Are thepurplebooth Ltd. Company. All Rights Reserved | Site Map | <a href="#">Terms & Conditions</a> | <a href="#">Privacy Policy</a> | <a href="#">Cookie Policy</a>
+				</p>
+			</div>
+		</div>
+	
     </div>
 </div>
 <!-- FOOTER -->
