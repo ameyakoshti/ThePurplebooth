53ac2a9f6436e3d9f58687690d1ae3c8edf15049
anetfreak <anetfreak@gmail.com>
2013-09-03 18:23:55 -0700

    Streamlining the other images on the homepage.. looks nice..

--- index.php
+++ index.php
@@ -57,83 +57,91 @@ include $_SERVER['DOCUMENT_ROOT'] . '/codenameDS/includes/links.php';
 			</div>
 			<!-- /.carousel -->
 			<div id="category-heading">
-			<div class="row-fluid">
-				<div class="span3"><h3 class="featurette-heading" style="font-family: Raleway;">Image Categories</h3></div>
-				<div class="span9" style="margin: 10px 0px 30px 0px"><hr class="featurette-divider"></div>
-			</div>
+				<div class="row-fluid">
+					<div class="span3"><h3 class="featurette-heading" style="font-family: Raleway;">Image Categories</h3></div>
+					<div class="span9" style="margin: 10px 0px 30px 0px"><hr class="featurette-divider"></div>
+				</div>
 			</div>
 			<div id="category-container">
-			<div class="row-fluid">
-				<div class="category-row span3">
-					<a href="/codenameDS/gallery.php?category=abstract&project=all&userid=all"><img src="/codenameDS/img/home_catergories/abstract.jpg"></a>
-					<p>Abstract</p>
-				</div>
-				<div class="category-row span3">
-					<a href="/codenameDS/gallery.php?category=landscape&project=all&userid=all"><img src="/codenameDS/img/home_catergories/landscape.jpg"></a>
-					<p>Landscape</p>
-				</div>
-				<div class="category-row span3">
-					<a href="/codenameDS/gallery.php?category=street&project=all&userid=all"><img src="/codenameDS/img/home_catergories/street.jpg"></a>
-					<p>Street</p>
-				</div>
-				<div class="category-row span3">
-					<a href="/codenameDS/gallery.php?category=portrait&project=all&userid=all"><img src="/codenameDS/img/home_catergories/portrait.jpg"></a>
-					<p>Portrait</p>
-				</div>
-				<div class="category-row span3">
-					<a href="/codenameDS/gallery.php?category=wildlife&project=all&userid=all"><img src="/codenameDS/img/home_catergories/wildlife.jpg"></a>
-					<p>Wildlife</p>
-				</div>
+				<div class="row-fluid">
+					<div class="span1"></div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=abstract&project=all&userid=all"><img src="/codenameDS/img/home_catergories/abstract.jpg"></a>
+						<p>Abstract</p>
+					</div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=landscape&project=all&userid=all"><img src="/codenameDS/img/home_catergories/landscape.jpg"></a>
+						<p>Landscape</p>
+					</div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=street&project=all&userid=all"><img src="/codenameDS/img/home_catergories/street.jpg"></a>
+						<p>Street</p>
+					</div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=portrait&project=all&userid=all"><img src="/codenameDS/img/home_catergories/portrait.jpg"></a>
+						<p>Portrait</p>
+					</div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=wildlife&project=all&userid=all"><img src="/codenameDS/img/home_catergories/wildlife.jpg"></a>
+						<p>Wildlife</p>
+					</div>
 				</div>
 				<div class="row-fluid">
-				<div class="category-row span2">
-					<a href="/codenameDS/gallery.php?category=architecture&project=all&userid=all"><img src="/codenameDS/img/home_catergories/architecture.jpg"></a>
-					<p>Architecture</p>
+					<div class="span1"></div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=architecture&project=all&userid=all"><img src="/codenameDS/img/home_catergories/architecture.jpg"></a>
+						<p>Architecture</p>
+					</div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=macro&project=all&userid=all"><img src="/codenameDS/img/home_catergories/macro.jpg"></a>
+						<p>Macro</p>
+					</div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=hdr&project=all&userid=all"><img src="/codenameDS/img/home_catergories/hdr.jpg"></a>
+						<p>HDR</p>
+					</div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=event&project=all&userid=all"><img src="/codenameDS/img/home_catergories/event.jpg"></a>
+						<p>Event</p>
+					</div>
+					<div class="category-row span2">
+						<a href="/codenameDS/gallery.php?category=product&project=all&userid=all"><img src="/codenameDS/img/home_catergories/product.jpg"></a>
+						<p>Product</p>
+					</div>
 				</div>
-				<div class="category-row span2">
-					<a href="/codenameDS/gallery.php?category=macro&project=all&userid=all"><img src="/codenameDS/img/home_catergories/macro.jpg"></a>
-					<p>Macro</p>
-				</div>
-				<div class="category-row span2">
-					<a href="/codenameDS/gallery.php?category=hdr&project=all&userid=all"><img src="/codenameDS/img/home_catergories/hdr.jpg"></a>
-					<p>HDR</p>
-				</div>
-				<div class="category-row span2">
-					<a href="/codenameDS/gallery.php?category=event&project=all&userid=all"><img src="/codenameDS/img/home_catergories/event.jpg"></a>
-					<p>Event</p>
-				</div>
-				<div class="category-row span2">
-					<a href="/codenameDS/gallery.php?category=product&project=all&userid=all"><img src="/codenameDS/img/home_catergories/product.jpg"></a>
-					<p>Product</p>
-				</div>
-			</div>
 			</div>
 			
 			<div id="photographers-heading">
-				<div class="span4"><h3 class="featurette-heading" style="font-family: Raleway;">Featured Photographers</h3></div>
-				<div class="span8" style="margin: 10px 0px 30px 0px"><hr class="featurette-divider"></div>
+				<div class="row-fluid">
+					<div class="span4"><h3 class="featurette-heading" style="font-family: Raleway;">Featured Photographers</h3></div>
+					<div class="span8" style="margin: 10px 0px 30px 0px"><hr class="featurette-divider"></div>
+				</div>
 			</div>
 			<div id="photographers-container">
-				<div style="margin: 20px 20px 20px 50px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
+				<div class="row-fluid">
+					<div class="span1"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
 				</div>
 			</div>
 			
 			<div id="projects-heading">
-				<div class="span3"><h3 class="featurette-heading" style="font-family: Raleway;">Featured Editors</h3></div>
-				<div class="span9" style="margin: 10px 0px 30px 0px"><hr class="featurette-divider"></div>
+				<div class="row-fluid">
+					<div class="span3"><h3 class="featurette-heading" style="font-family: Raleway;">Featured Editors</h3></div>
+					<div class="span9" style="margin: 10px 0px 30px 0px"><hr class="featurette-divider"></div>
+				</div>
 			</div>
 			<div id="projects-container">
-				<div style="margin: 20px 20px 20px 50px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
-					<img src="img/200X150.gif" class="img-polaroid" style="margin-left:10px;">
+				<div class="row-fluid">
+					<div class="span1"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
+					<div class="span2"><img src="img/200X150.gif"></div>
 				</div>
 			</div>
 
