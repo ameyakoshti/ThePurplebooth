2fb4de409d91a9e25cace5d08b4144e7770f901c
donsalari <ameyakoshti@gmail.com>
2013-09-11 19:49:25 -0700

    database file updated

--- codenameDS.sql
+++ codenameDS.sql
@@ -1,26 +1,18 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 3.5.7
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 03, 2013 at 12:21 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.16
+-- Host: localhost
+-- Generation Time: Sep 12, 2013 at 04:48 AM
+-- Server version: 5.5.29
+-- PHP Version: 5.4.10
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Database: `codenameds`
+-- Database: `codenameDS`
 --
-CREATE DATABASE IF NOT EXISTS `codenameds` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `codenameds`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +20,7 @@ USE `codenameds`;
 -- Table structure for table `editrequest`
 --
 
-CREATE TABLE IF NOT EXISTS `editrequest` (
+CREATE TABLE `editrequest` (
   `request_id` int(11) NOT NULL AUTO_INCREMENT,
   `request_user_id` int(11) unsigned NOT NULL,
   `request_image_user_id` int(11) unsigned NOT NULL,
@@ -37,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `editrequest` (
   `request_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`request_id`),
   KEY `request_user_id` (`request_user_id`,`request_image_id`,`request_image_user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=76 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -45,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `editrequest` (
 -- Table structure for table `imagecomment`
 --
 
-CREATE TABLE IF NOT EXISTS `imagecomment` (
+CREATE TABLE `imagecomment` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
   `comment_user_id` int(11) unsigned NOT NULL,
   `comment_text` varchar(200) NOT NULL,
@@ -58,11 +50,8 @@ CREATE TABLE IF NOT EXISTS `imagecomment` (
   KEY `comment_user_id_2` (`comment_user_id`),
   KEY `comment_image_id_2` (`comment_image_id`),
   KEY `comment_timestamp` (`comment_timestamp`),
-  KEY `comment_user_name` (`comment_user_name`),
-  KEY `comment_user_id_3` (`comment_user_id`),
-  KEY `comment_image_id_3` (`comment_image_id`),
-  KEY `comment_user_name_2` (`comment_user_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `comment_user_name` (`comment_user_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 -- --------------------------------------------------------
 
@@ -70,15 +59,15 @@ CREATE TABLE IF NOT EXISTS `imagecomment` (
 -- Table structure for table `imageinfo`
 --
 
-CREATE TABLE IF NOT EXISTS `imageinfo` (
+CREATE TABLE `imageinfo` (
   `image_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
-  `partner_id` int(11) unsigned DEFAULT NULL,
+  `editor_id` int(11) DEFAULT NULL,
   `name` varchar(45) NOT NULL,
   `type` varchar(45) DEFAULT NULL,
   `size` int(11) NOT NULL,
   `content` longblob NOT NULL,
-  `edited_image` longblob,
+  `edited_img_link` varchar(45) DEFAULT NULL,
   `title` varchar(45) DEFAULT NULL,
   `description` varchar(200) DEFAULT NULL,
   `category` varchar(45) DEFAULT NULL,
@@ -88,10 +77,28 @@ CREATE TABLE IF NOT EXISTS `imageinfo` (
   `closed_project` int(1) DEFAULT NULL,
   PRIMARY KEY (`image_id`),
   KEY `user_id` (`user_id`),
-  KEY `partner_id` (`partner_id`),
-  KEY `partner_id_2` (`partner_id`),
   KEY `user_id_2` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=39 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `notification_id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_user_id` int(11) unsigned NOT NULL,
+  `to_user_id` int(11) unsigned NOT NULL,
+  `notification_image_id` int(11) NOT NULL,
+  `notification_type` enum('COMMENT','REPLY','REQUEST_NEW','REQUEST_APPROVED','REQUEST_DENIED') NOT NULL,
+  `notification_read` tinyint(1) NOT NULL DEFAULT '0',
+  `notification_timestamp` datetime NOT NULL,
+  PRIMARY KEY (`notification_id`),
+  KEY `from_user_id` (`from_user_id`,`to_user_id`),
+  KEY `notification_image_id` (`notification_image_id`),
+  KEY `to_user_id` (`to_user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 -- --------------------------------------------------------
 
@@ -99,7 +106,7 @@ CREATE TABLE IF NOT EXISTS `imageinfo` (
 -- Table structure for table `replycomment`
 --
 
-CREATE TABLE IF NOT EXISTS `replycomment` (
+CREATE TABLE `replycomment` (
   `reply_id` int(11) NOT NULL AUTO_INCREMENT,
   `reply_user_id` int(11) unsigned NOT NULL,
   `reply_user_name` varchar(45) NOT NULL,
@@ -110,10 +117,8 @@ CREATE TABLE IF NOT EXISTS `replycomment` (
   KEY `reply_user_id` (`reply_user_id`),
   KEY `reply_comment_id` (`reply_comment_id`),
   KEY `reply_user_name` (`reply_user_name`),
-  KEY `reply_user_name_2` (`reply_user_name`),
-  KEY `reply_user_id_2` (`reply_user_id`),
-  KEY `reply_comment_id_2` (`reply_comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `reply_user_name_2` (`reply_user_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -121,19 +126,17 @@ CREATE TABLE IF NOT EXISTS `replycomment` (
 -- Table structure for table `userrating`
 --
 
-CREATE TABLE IF NOT EXISTS `userrating` (
+CREATE TABLE `userrating` (
   `rating_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
   `user_name` varchar(45) NOT NULL,
   `stars` int(1) NOT NULL,
   `reviews` varchar(200) NOT NULL,
-  `rated_by_user_id` int(11) unsigned NOT NULL,
+  `rated_by_user_id` int(11) NOT NULL,
   `rated_by_user_name` varchar(45) NOT NULL,
   PRIMARY KEY (`rating_id`),
-  KEY `user_id_idx` (`user_id`),
-  KEY `user_id` (`user_id`),
-  KEY `rated_by_user_id` (`rated_by_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `user_id_idx` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -141,7 +144,7 @@ CREATE TABLE IF NOT EXISTS `userrating` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(45) NOT NULL,
   `full_name` varchar(45) DEFAULT NULL,
@@ -155,43 +158,24 @@ CREATE TABLE IF NOT EXISTS `users` (
   `class` varchar(25) NOT NULL,
   `creativity` varchar(10) NOT NULL,
   `rating` varchar(10) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `imagecomment`
+-- Constraints for table `notifications`
 --
-ALTER TABLE `imagecomment`
-  ADD CONSTRAINT `imagecomment_ibfk_1` FOREIGN KEY (`comment_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `imagecomment_ibfk_2` FOREIGN KEY (`comment_image_id`) REFERENCES `imageinfo` (`image_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `imageinfo`
---
-ALTER TABLE `imageinfo`
-  ADD CONSTRAINT `imageinfo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `imageinfo_ibfk_2` FOREIGN KEY (`partner_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `replycomment`
---
-ALTER TABLE `replycomment`
-  ADD CONSTRAINT `replycomment_ibfk_1` FOREIGN KEY (`reply_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `replycomment_ibfk_2` FOREIGN KEY (`reply_comment_id`) REFERENCES `imagecomment` (`comment_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notifications_ibfk_3` FOREIGN KEY (`notification_image_id`) REFERENCES `imageinfo` (`image_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `userrating`
 --
 ALTER TABLE `userrating`
   ADD CONSTRAINT `rated_by_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `userrating_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `userrating_ibfk_2` FOREIGN KEY (`rated_by_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
