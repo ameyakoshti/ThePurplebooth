b316105807548f5dd58a11496712f20fbaf6d5cb
donsalari <ameyakoshti@gmail.com>
2013-09-18 00:21:30 -0700

    goto link fixed

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -9,11 +9,10 @@
 //			};
 
 	$(document).ready(function(){
-//		$('.goToImage').click(function(event){
-//			event.preventDefault();
-//			document.location.href='http://localhost:8888/codenameDS/selected_image.php?image_id='+$(this).data('imgid');
-//		});
-
+		$('.goToImage').click(function(event){
+			event.preventDefault();
+			document.location.href='http://localhost:8888/codenameDS/selected_image.php?image_id='+$(this).data('imgid');
+		});
 	}
 	
 );
